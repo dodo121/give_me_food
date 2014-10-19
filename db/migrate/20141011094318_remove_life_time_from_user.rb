aef51cae0ad571d731bdb8dc7cf1d9fe6e2d0886
Dodo <dodox360@gmail.com>
2014-10-19 17:40:51 +0200

    delete

--- db/migrate/20141011094318_remove_life_time_from_user.rb
+++
@@ -1,4 +0,0 @@
-class RemoveLifeTimeFromUser < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20141011095507_remove_column_life_time_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnLifeTimeFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :life_time, :integer
-  end
-end
