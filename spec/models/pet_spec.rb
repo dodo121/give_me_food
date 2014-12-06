69d5cffd1152f91b712510d0137f71c3e6035041
dodo121 <dszromik@getso.pl>
2014-12-06 14:36:33 +0100

    factories update

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe Pet do
   it "is invalid without name" do
-    expect( FactoryGirl.build(:pet, name:nil) ).not_to be_valid
+    expect( build(:pet, name:nil) ).not_to be_valid
   end
   it "calculates life time" do
-    expect( FactoryGirl.create(:pet, date_of_birth: Date.today - 10.day).life_time).to eq 10
+    expect( create(:pet, date_of_birth: Date.today - 10.day).life_time).to eq 10
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 describe User do
   it "has a valid factory" do
-    expect( FactoryGirl.create(:user) ).to be_valid
+    expect( create(:user) ).to be_valid
   end
   it "is invalid without a email" do
-    expect( FactoryGirl.build(:user, email: nil) ).not_to be_valid
+    expect( build(:user, email: nil) ).not_to be_valid
   end
   it "is invalid without password" do
-    expect( FactoryGirl.build(:user, password: nil)).not_to be_valid
+    expect( build(:user, password: nil)).not_to be_valid
   end
   it "has unique email adress" do
-    FactoryGirl.create(:user, email: 'johndoe1@gmail.com')
-    expect( FactoryGirl.build(:user, email: 'johndoe1@gmail.com') ).not_to be_valid
+    create(:user, email: 'johndoe1@gmail.com')
+    expect( build(:user, email: 'johndoe1@gmail.com') ).not_to be_valid
   end
 end
