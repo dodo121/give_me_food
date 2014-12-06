831ee2cc6213635b3c0cc4058d165bf37255d44d
dodo121 <dszromik@getso.pl>
2014-12-06 12:12:15 +0100

    seed data update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+user = User.create(email: 'johndoe@foo.com', password: 'pass1234')
+
 (1..10).each do |n| 
-  Pet.create(name: "Pixel" + "#{n}", date_of_birth: '2014-10-10')
+  Pet.create(name: 'Pixel' + "#{n}", date_of_birth: '2014-10-10', user: user)
 end
+
+Event.create(title: 'Vet visit', description: 'Lorem ipsum sit dolot ament', date: '2014-12-30', time: '12:00', user: user, pet: Pet.first)
+
