ab7e41b0843f5afa02743095f39ae774bab23389
dszromik <dominik.szromik@polcode.net>
2015-03-06 12:21:36 +0100

    adds description and lat/long for map

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231090932) do
+ActiveRecord::Schema.define(version: 20150306112109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 20141231090932) do
     t.string   "country"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "description"
   end
 
   create_table "users", force: :cascade do |t|
