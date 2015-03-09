6f2f82302bfc93c4ffbbc1bfea06c0a52abe6ed7
dszromik <dominik.szromik@polcode.net>
2015-03-09 14:25:30 +0100

    Add nice flash messages
    Add nice flash messages

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -18,6 +18,6 @@ class ChargesController < ApplicationController
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to edit_user_registration_path
+    redirect_to new_charge_path
   end
 end
