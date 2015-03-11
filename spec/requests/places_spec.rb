5c50bdb08f57932f621320a218b59bfd96e50f82
dszromik <dominik.szromik@polcode.net>
2015-03-11 16:12:47 +0100

    places request spec update

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Places", :type => :request do
-  
-  context "Signed in User" do
-    
+
+  describe "Signed in User" do
+    let!(:user) { create(:user) }
+    let!(:place) { create(:place) }
+
     before do
-      user = create(:user) 
-      @place = create(:place)
       sign_in_action user
     end
-    
-    describe "can" do
-      it "add place to visited and increment @place users by 1" do
+
+    context "can" do
+      it "add place to visited and increment place users by 1" do
         expect {
-          add_place_to_visited @place
-        }.to change(@place.users, :count).by(1)
+          add_place_to_visited place
+        }.to change(place.users, :count).by(1)
       end
-  
+
       it "see place marked as visited" do
-        add_place_to_visited @place
+        add_place_to_visited place
         visit places_path
         expect(page).to have_content("Visited")
       end
     end
-    
-    describe "can't" do
+
+    context "can't" do
       it "mark place as visited twice" do
-        add_place_to_visited @place
+        add_place_to_visited place
         expect {
-          add_place_to_visited @place
-        }.to change(@place.users, :count).by(0)
+          add_place_to_visited place
+        }.to change(place.users, :count).by(0)
         expect(page).to have_content("You can't mark place as visited twice")
-      end    
-    end 
+      end
+    end
   end
 end
--- spec/support/user_macros.rb
+++ spec/support/user_macros.rb
@@ -1,17 +1,17 @@
 module UserMacros
-  
+
   def sign_in_action(user)
     visit root_path
     click_link 'sign in'
     fill_in 'user_login', with: user.email
     fill_in 'Password', with: user.password
-    click_button 'Log in'
+    click_button 'Sign in'
   end
-  
+
   def add_place_to_visited(place)
     visit places_path
     click_link 'Show'
-    expect(current_path).to eq place_path(place) 
+    expect(current_path).to eq place_path(place)
     click_link 'Add to visited', place
     expect(current_path).to eq place_path(place)
   end
