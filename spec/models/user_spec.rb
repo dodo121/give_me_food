384bdac1b2bd2db5de09ea8b433a31239c379a57
dszromik <dominik.szromik@polcode.net>
2015-03-11 15:37:11 +0100

    user_spec update and new tests for premium methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,34 @@
 require 'rails_helper'
+require 'shoulda/matchers'
 
 describe User do
   it "has a valid factory" do
     expect( create(:user) ).to be_valid
   end
-  it "is invalid without a email" do
-    expect( build(:user, email: nil) ).not_to be_valid
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:password) }
+
+  describe '.has_premium_account?' do
+    it 'returns true if user has active premium account' do
+      expect( create(:user, premium_expiry_date: Time.now + (2*7*24*60*10)).has_premium_account? ).to be(true)
+    end
+    it 'returns false if user premium account exipred' do
+      expect( create(:user, premium_expiry_date: Time.now - (2*7*24*60*10)).has_premium_account? ).to be(false)
+    end
   end
-  it "is invalid without password" do
-    expect( build(:user, password: nil)).not_to be_valid
-  end
-  it "has unique email adress" do
-    create(:user, email: 'johndoe1@gmail.com')
-    expect( build(:user, email: 'johndoe1@gmail.com') ).not_to be_valid
+
+  describe '.make_premium' do
+    Timecop.freeze
+    let(:user){ create(:user) }
+
+    before do
+      user.make_premium(1)
+    end
+
+    it 'sets User premium_expiry_date' do
+      expect( user.premium_expiry_date ).to eq(Time.now + 1.month)
+    end
   end
 end
